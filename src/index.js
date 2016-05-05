74573b1e39351346d6a79ece0350f38fb1b4acd9
alan bount <alan@zeroasterisk.com>
2016-05-05 15:58:52 -0400

    add buildValidateFromSimpleSchema code

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
-export const sum = (a, b) => {
-  return new Promise(resolve => resolve(a + b));
-};
+// A helper to turn a Meteor SimpleSchema
+//     https://github.com/aldeed/meteor-simple-schema
+// into a validate function for use with ReduxForm (et. al.)
+//     http://redux-form.com/
+
+import _ from 'lodash';
+
+/**
+ * this is a validation function based on SimpleSchema,
+ * passed into redux-form as `validate`
+ * @param object mySchemaObject eg: new SimpleSchema({...})
+ * @return object errors eg: {} = valid, or {field: "reason"} = invalid
+ */
+export const buildValidateFromSimpleSchema = (mySchemaObject, contextName) => {
+  return (values) => {
+    const context = mySchemaObject.namedContext(contextName ? contextName : 'myContext');
+    if (context.validate(values)) {
+      // isValid
+      return {};
+    }
+    return _.fromPairs(_.map(
+      context.invalidKeys(),
+      (o) => [o.name, context.keyErrorMessage(o.name)]
+    ));
+  }
+}
+
+export default buildValidateFromSimpleSchema;
+
