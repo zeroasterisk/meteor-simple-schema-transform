6f4b522533a833647af0ea46fc21c4f7141dd855
alan bount <alan@zeroasterisk.com>
2016-05-12 09:14:47 -0400

    fixed broken ES6 exports

--- src/index.js
+++ src/index.js
@@ -1,9 +1,6 @@
 import * as buildClean from './build-clean.js';
 import * as forReduxForm from './for-redux-form.js';
 
-export buildClean;
-export forReduxForm;
-
 export const SimpleSchemaTransform = {
   buildClean,
   forReduxForm,
